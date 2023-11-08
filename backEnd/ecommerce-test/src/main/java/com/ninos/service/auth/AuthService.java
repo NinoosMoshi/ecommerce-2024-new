c540cb0096dedcbbb9088582df483d70861440df
NinoosMoshi <nalkaton@gmail.com>
2023-11-08 09:58:27 -0600

    add registration

---
+++ backEnd/ecommerce-test/src/main/java/com/ninos/service/auth/AuthService.java
@@ -0,0 +1,9 @@
+package com.ninos.service.auth;
+
+import com.ninos.model.dto.RegisterDTO;
+
+public interface AuthService {
+
+    String register(RegisterDTO registerDTO);
+
+}
---
+++ backEnd/ecommerce-test/src/main/java/com/ninos/service/auth/AuthServiceImpl.java
@@ -0,0 +1,64 @@
+package com.ninos.service.auth;
+
+import lombok.AllArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.ninos.exception.ProjectApiException;
+import com.ninos.model.dto.RegisterDTO;
+import com.ninos.model.entity.Role;
+import com.ninos.model.entity.User;
+import com.ninos.repository.RoleRepository;
+import com.ninos.repository.UserRepository;
+
+@Service
+@AllArgsConstructor
+public class AuthServiceImpl implements AuthService{
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+
+    @Override
+    public String register(RegisterDTO registerDTO) {
+
+        // check if username is exists in database
+        if(userRepository.existsByUsername(registerDTO.getUsername())){
+            throw new ProjectApiException(HttpStatus.BAD_REQUEST, "Username is already exists");
+        }
+
+        // check if email is exists in database
+        if(userRepository.existsByEmail(registerDTO.getEmail())){
+            throw new ProjectApiException(HttpStatus.BAD_REQUEST, "Email is already exists");
+        }
+
+        User user = new User();
+        user.setName(registerDTO.getName());
+        user.setUsername(registerDTO.getUsername());
+        user.setEmail(registerDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        roles.add(userRole);
+
+        user.setRoles(roles);
+
+
+        userRepository.save(user);
+
+        return "User Registered Successfully";
+    }
+
+
+
+
+}
